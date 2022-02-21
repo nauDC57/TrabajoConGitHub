46ae5fcba3a80bf843a235a1fd46a79752f69184
Nauzet Dorta Carvajal <naudc57@gmail.com>
2022-02-21 13:19:54 +0000

    20220221(español)

--- TrabajoConGitHub/Program.cs
+++ TrabajoConGitHub/Program.cs
@@ -6,7 +6,7 @@ namespace TrabajoConGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo!");
         }
     }
 }
